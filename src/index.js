25a7b0d75f1fdfb5f0c0a9d8070319a74cf1b94d
Amar Hamlaoui <amarhamlaoui@gmail.com>
2022-03-01 19:25:15 +0100

    Add min height to leaderboard list.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-import './style.css';
+import './style.scss';
+import Leaderboard from './modules/leaderboard.js';
+
+const leaderboard = new Leaderboard();
+
+leaderboard.display();
+
+const refresh = document.querySelector('#refresh');
+const add = document.querySelector('#submit');
+
+refresh.addEventListener('click',()=>{
+  leaderboard.display();
+  console.log('refresh');
+});
+
+add.addEventListener('click',()=>{
+  const name = document.querySelector('#name');
+  const score = document.querySelector('#score');
+
+  const scoreItem = {
+    name: name.value,
+    score: score.value
+  }
+
+  leaderboard.add(scoreItem);
+})
